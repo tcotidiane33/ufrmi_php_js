e0ad6086155520e5349b6bc44c007ab2ce3604b1
tcotidiane <51900661+tcotidiane33@users.noreply.github.com>
2022-07-14 01:14:46 +0200

    update v2

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,4 @@
-
-<?php if($_SESSION['id'])
-{ ?><div class="brand clearfix ">
+<?php if ($_SESSION['id']) { ?><div class="brand clearfix ">
 		<a href="/index.php" class="logo" style="font-size:16px;">Ufrmi</a>
 		<span class="menu-btn"><i class="fa fa-bars"></i></span>
 		<ul class="ts-profile-nav">
@@ -16,37 +14,38 @@
 
 <?php
 } else { ?>
-<div class="brand clearfix ">
-	<nav class="">
-		
-		<ul class="ts-sidebar-menu" style="display:flex;align-items: center;justify-content: center;">
-		
-		<li class="ts-label">
-			<a href="#" class="logo" style="font-size:16px;">Ufrmi</a>
-		<span class="menu-btn"><i class="fa fa-bars"></i></span>
-		</li>
+	<div class="brand clearfix ">
+		<nav class="">
 
-		<?PHP if (isset($_SESSION['id'])) { ?>
-			<li><a href="index.php"><i class="fa fa-desktop"></i>Home</a></li>
-			<li><a href="parcours.php"><i class="fa fa-user"></i> Parcours</a></li>
-			<li><a href="labs.php"><i class="fa fa-files-o"></i>Labs</a></li>
-			<li><a href="space.php"><i class="fa fa-file-o"></i>Espaces Etudiants</a></li>
-			<li><a href="forum.php"><i class="fa fa-file-o"></i>forums</a></li>
-			<!-- <li><a href="access-log.php"><i class="fa fa-file-o"></i>Access</a></li> -->
-		<?php
-		} else { ?>
+			<ul class="ts-sidebar-menu" style="display:flex;align-items: center;justify-content: center;">
 
-			<li><a href="index.php"><i class="fa fa-files-o"></i>Home</a></li>
-			<li><a href="parcours.php"><i class="fa fa-users"></i> Parcours</a></li>
-			<li><a href="admin/labs.php"><i class="fa fa-user"></i> Labs</a></li>
-			<li><a href="admin/space.php"><i class="fa fa-user"></i> Espaces Etudiants</a></li>
-			<li><a href="admin/about.php"><i class="fa fa-user"></i> About</a></li>
-		<?php
-		} ?>
+				<li class="ts-label">
+					<a href="#" class="logo" style="font-size:16px;">Ufrmi</a>
+					<!-- <span class="menu-btn"><i class="fa fa-bars"></i></span> -->
+				</li>
 
-		<li class="ts-label header-label"><a href="admin/login.php">Connexion</li>
-	</ul>
-	</nav>
-		
+				<?PHP if (isset($_SESSION['id'])) { ?>
+					<li><a href="index.php"><i class="fa fa-desktop"></i>Home</a></li>
+					<li><a href="parcours.php"><i class="fa fa-user"></i> Parcours</a></li>
+					<li><a href="labs.php"><i class="fa fa-files-o"></i>Labs</a></li>
+					<li><a href="space.php"><i class="fa fa-file-o"></i>Espaces Etudiants</a></li>
+					<li><a href="galerie.php"><i class="fa fa-file-o"></i>Galerie</a></li>
+					<!-- <li><a href="access-log.php"><i class="fa fa-file-o"></i>Access</a></li> -->
+				<?php
+				} else { ?>
+
+					<li><a href="index.php"><i class="fa fa-desktop"></i>Home</a></li>
+					<li><a href="parcours.php"><i class="fa fa-users"></i> Parcours</a></li>
+					<li><a href="admin/labs.php"><i class="fa fa-user"></i> Labs</a></li>
+					<li><a href="admin/space.php"><i class="fa fa-user"></i> Espaces Etudiants</a></li>
+					<li><a href="admin/about.php"><i class="fa fa-user"></i> About</a></li>
+					<li><a href="galerie.php"><i class="fa fa-file-o"></i>Galerie</a></li>
+				<?php
+				} ?>
+
+				<li class="ts-label header-label"><a href="admin/login.php">Connexion</li>
+			</ul>
+		</nav>
+
 	</div>
-	<?php } ?>
+<?php } ?>
